7704520d1180b9ce801a970b651781751c4a7ca7
kingsoo123 <sabistore12@gmail.com>
2021-03-16 11:04:25 -0700

    added a back function

--- src/components/page/Secondary.js
+++ src/components/page/Secondary.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
 import Button from "../Button";
 
-
 const Secondary = (props) => {
+  let history = useHistory();
+
   const [category, setCategory] = useState("");
-  const [jokes, setJokes] = useState(['No matching Joke found'])
+  const [jokes, setJokes] = useState(["No matching Joke found"]);
   useEffect(() => {
     setCategory(props?.match?.params?.category);
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch(
       `https://v2.jokeapi.dev/joke/${props?.match?.params?.category}?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&idRange=0-300&amount=10`
     )
@@ -18,64 +20,70 @@ const Secondary = (props) => {
         setJokes(data?.jokes);
       })
       .catch((error) => console.log(error));
-  },[])
+  }, []);
 
   console.log(jokes);
 
-  const secPageJokes = jokes.map((joke, i)=>{
-    return(
+  const secPageJokes = jokes.map((joke, i) => {
+    return (
       <div key={i}>
-
-      <div class="card">
-        <div class="card-header">Type: {joke?.type}</div>
-       {joke?.type === 'single'? <div class="card-body">
-          <p class="card-text">
-         {joke?.joke}
-          </p>
-          <a href="#" class="btn btn-outline-primary">
-           {joke?.lang ? 'English' : null}
-          </a>
-        </div> : <div class="card-body">
-          <p class="card-text">
-         {joke?.setup}
-          </p>
-          <p class="card-text">
-         Delivery: {joke?.delivery}
-          </p>
-          <a href="#" class="btn btn-outline-primary">
-           {joke?.lang ? 'English' : null}
-          </a>
-        </div>}
+        <div class="card">
+          <div class="card-header">Type: {joke?.type}</div>
+          {joke?.type === "single" ? (
+            <div class="card-body">
+              <p class="card-text">{joke?.joke}</p>
+              <a href="#" class="btn btn-outline-primary">
+                {joke?.lang ? "English" : null}
+              </a>
+            </div>
+          ) : (
+            <div class="card-body">
+              <p class="card-text">{joke?.setup}</p>
+              <p class="card-text">Delivery: {joke?.delivery}</p>
+              <a href="#" class="btn btn-outline-primary">
+                {joke?.lang ? "English" : null}
+              </a>
+            </div>
+          )}
+        </div>
       </div>
+    );
+  });
 
-      </div>
-    )
-  })
-
-
-  const randomizeJoke = ()=>{
+  const randomizeJoke = () => {
     fetch(
       `https://v2.jokeapi.dev/joke/${props?.match?.params?.category}?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&idRange=0-300&amount=10`
     )
       .then((response) => response.json())
       .then((data) => {
-        setJokes(data?.jokes.sort( () => .5 - Math.random() ));
+        setJokes(data?.jokes.sort(() => 0.5 - Math.random()));
       })
       .catch((error) => console.log(error));
-  }
+  };
   return (
     <>
       <div
         className="col-md-12 col-sm-12 pb-1"
         style={{ background: "#0069D9" }}
       >
+        <p
+          className="back"
+          onClick={() => {
+            history.push("/");
+          }}
+        >
+          Go Back
+        </p>
         <h1 className="text-center">{category} Jokes</h1>
       </div>
 
-     {secPageJokes}
+      {secPageJokes}
 
-     <div className="d-flex justify-content-center p-4">
-        <Button randomizeJoke={randomizeJoke} name = {props?.match?.params?.category}/>
+      <div className="d-flex justify-content-center p-4">
+        <Button
+          randomizeJoke={randomizeJoke}
+          name={props?.match?.params?.category}
+        />
       </div>
     </>
   );
